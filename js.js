6d1fc0c558a65ed3783e8b9f0b5d3c7e84612281
misha reshetnyak <mihareshetnyak@gmail.com>
2014-08-03 14:45:58 +0300

    bootstrap and capitalize

--- js.js
+++ js.js
@@ -1,14 +1,13 @@
 function countMoney() {
-
   var percent = parseFloat(document.getElementById("percent").value);
   var amount = parseFloat(document.getElementById("amount").value);
   var incr = parseFloat(document.getElementById("incr").value);
   var html = "<ol>"
+  var check = document.getElementById("yes");
+    //alert(check);
   for(var j = 1; j <= 12; j++){
     amount += amount / 100 * percent;
-    percent += incr;
     html += "<li>" + amount + "</li>"
-  }
-  html = html +"</ol>";
-  document.getElementById('result').innerHTML = html;
+    if (check.checked) {
+      percent += incr;
 }
